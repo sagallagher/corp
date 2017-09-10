36909b7c414f55f431894575daa558264eaf2f7b
Austin Gladfelter <agladfelter@mail.bradley.edu>
2017-09-09 19:57:34 -0500

    Add files via upload
    Uses generic variables until data from parser can be obtained. Some code needs to be added to main for this.

---
+++ Matrix.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <stdio.h>
+
+template <class T>
+class Matrix
+{
+public:
+	Matrix(int rows, int cols)
+	{
+		setRow(rows);
+		setCol(cols);
+
+		data = new T*[rows];
+
+		for (int i = 0; i < row; i++) {
+			data[i] = new T[cols]; 
+		}
+
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < cols; j++) {
+				data[i][j] = (T)i * j;
+			}
+		}
+	}
+
+	void setRow(int r) { row = r; }
+	void setCol(int c) { col = c; }
+	T& operator()(int row, int col);
+private:
+	T **data;
+	int row, col;
+};
+
+
+
+template<class T> T& Matrix<T>::operator()(int row, int col)
+{
+	return data[row][col];
+}
