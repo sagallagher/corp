3101d3c3f0fbdc8ea4f633ea1ef476a52e42981d
steven <sagallagher@mail.bradley.edu>
2017-09-19 19:24:45 -0500

    bug: pull(string,string) calling pull(string,bool)

--- CORP/UNIT_TEST/OptionContainer/TEST_OptionContainer.cpp
+++ CORP/UNIT_TEST/OptionContainer/TEST_OptionContainer.cpp
@@ -1,61 +1,62 @@
 
-#include "CppUnitTest.h"
-#include "../../CORP/OptionContainer/OptionContainer.h"
+#include"CppUnitTest.h"
+#include"../../CORP/OptionContainer/OptionContainer.h"
+#include<iostream>
 
-std::string generateKey(int keyNumber) { return "key" + keyNumber; }
+std::string generateKey(int keyNumber, std::string& tag) { return ("key" + tag).c_str() + keyNumber;  }
 
 
 bool PushPullTest() {
-	OptionContainer Intcontainer;
+	OptionContainer container;
+
+	std::string int_tag = "int";
+	std::string bool_tag = "bool";
+	std::string string_tag = "string";
 
 	// fill container with keys associated with int values
-	// all keys will have value of 0
-	for (int i = 0; i < 100; i++) {
-		Intcontainer.push(generateKey(i), "1");
+	// all keys will have value of 1
+	for (int i = 0; i < 10; i++) {
+		container.push(generateKey(i, int_tag), "1");
 	}
 	
 	// pull keys associated with int values
-	for (int i = 0; i < 100; i++) {
-		// make sure all values are false
-		if (1 != Intcontainer.pull(generateKey(i), 0)) {
+	for (int i = 1; i < 10; i++) {
+		// make sure all values are 1
+		if (container.pull(generateKey(i,int_tag), 0) != 1) {
+			std::cerr << "Error pulling int valuet\tExpected\t1\tActual:\t" << container.pull(generateKey(i, int_tag), 0) << std::endl;
 			return false;
 		}
 	}
-
-
-
-	OptionContainer boolContainer;
 
 	// fill container with keys associated with bool values
 	// all keys have value of true
-	for (int i = 0; i < 100; i++) {
-		boolContainer.push(generateKey(i), "true");
+	for (int i = 0; i < 10; i++) {
+		container.push(generateKey(i,bool_tag), "true");
+	}
+
+	// pull keys associated with bool values
+	for (int i = 0; i < 10; i++) {
+		// make sure all values are true
+		if (!container.pull(generateKey(i,bool_tag), false)) {
+			std::cerr << "Error pulling int valuet\tExpected\ttrue\tActual:\t" << container.pull(generateKey(i, bool_tag), false) << std::endl;
+			return false;
+		}
+	}
+
+	for (int i = 0; i < 10; i++) {
+		container.push(generateKey(i,string_tag), "string");
 	}
 
 	// pull keys associated with int values
-	for (int i = 0; i < 100; i++) {
+	// BUG: calling pull(string, bool); should be calling pull(string,string)
+	/*for (int i = 0; i < 10; i++) {
 		// make sure all values are 0
-		if (!boolContainer.pull(generateKey(i), false)) {
+		std::string no = "nsso";
+		if (container.pull(generateKey(i, string_tag), "not_string") == false) {
 			return false;
 		}
 	}
-
-
-	OptionContainer stringContainer;
-
-	for (int i = 0; i < 100; i++) {
-		stringContainer.push(generateKey(i), "yes");
-	}
-
-	/*// pull keys associated with int values
-	for (int i = 0; i < 100; i++) {
-		// make sure all values are 0
-		std::string no = "no";
-		if (stringContainer.pull(generateKey(i), "123123") != no) {
-			return false;
-		}
-	}*/
-
+	*/
 	return true;
 
 }
