c3432de5516a80f8304676cd80a30ef9ba2a7563
Seth Hostetler <shostetler@mail.bradley.edu>
2017-10-01 17:56:37 -0500

    Corrected files after GitHub confusion

--- CORP/CORP/main.cpp
+++ CORP/CORP/main.cpp
@@ -2,11 +2,29 @@
 #include"Config/Config.h"
 #include"OptionContainer/OptionContainer.h"
 
+#include <fstream>
+#include <string>
+
 Config* Config::_instance = 0;
 
 int main(int argc, char const *argv[]) {
 
 	Config::initialize(argc, argv);
+
+	//Open file stream for data
+	std::ifstream _is;
+
+	//How to correctly define input file?
+	std::string inputData = "../../data/24cell_facets.txt";
+	_is.open(inputData);
+
+	if (!_is)
+	{
+		std::cerr << "Unable to open file " << inputData << "for reading.";
+		return 1;
+	}
+
+	//End of file stream
 
 	return 0;
 }
