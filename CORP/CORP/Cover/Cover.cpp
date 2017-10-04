e7502502d29351d4bf1f629c756880598cdee77b
The-Alhooness <sarahkmcglasson@gmail.com>
2017-10-04 16:27:58 -0500

    Cover Class Base
    Created an empty class file and header to become the Cover Class

---
+++ CORP/CORP/Cover/Cover.cpp
@@ -0,0 +1,23 @@
+/**
+*  file:	Cover.cpp
+*  author:	Sarah McGlasson
+*  date:	10/1/2017
+*  updated:	10/4/2017
+*
+*  This class contains a boolset and takes a star object to control vertex selection
+*
+*/
+
+#include "Cover.h"
+#include "BoolSet.h"
+
+Cover::Cover()
+{
+
+}
+
+
+Cover::~Cover()
+{
+
+}
---
+++ CORP/CORP/Cover/Cover.h
@@ -0,0 +1,25 @@
+/**
+*  file:	Cover.h
+*  author:	Sarah McGlasson
+*  date:	10/1/2017
+*  updated:	10/4/2017
+*
+*  The header file for the Cover class
+*
+*/
+
+#ifndef _COVER_
+#define _COVER_
+
+class Cover
+{
+protected:
+
+
+public:
+	Cover();											//Constructor
+	~Cover();											//Destructor
+
+#endif
+
+};
