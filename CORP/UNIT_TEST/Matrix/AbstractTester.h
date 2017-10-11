d3ac40755bdc2cf8e5b52ae5b5344d3d80fdbe15
Austin Gladfelter <agladfelter@mail.bradley.edu>
2017-10-11 16:49:16 -0500

    Updates to Test_Matrix.hpp, deleted MatrixTester.hpp, added Abstract Tester
    Created AbstractTester.h for a more in depth unit testing experience. Considering making AbstractTester a more generic class so that it can be used in all of CORP's unit testing.

---
+++ CORP/UNIT_TEST/Matrix/AbstractTester.h
@@ -0,0 +1,44 @@
+#ifndef _ABSTRACT_TESTER_H
+#define _ABSTRACT_TESTER_H 1
+
+#include <iostream>
+#include <iostream>
+#include <map>
+#include <random>
+#include <string>
+
+class AbstractTester
+{
+protected:
+	std::map<std::string, int>			_errorCountMap;
+	std::string							_name;
+	std::ostream&						_os;
+
+public:
+	AbstractTester(const std::string& name = "", std::ostream& out = std::cout)
+		: _errorCountMap{}, _name{ name }, _os{ out }
+	{
+	}
+
+	virtual ~AbstractTester()
+	{
+		_errorCountMap.clear();
+	}
+
+
+	//Pure virtual "run every test" function
+	virtual void runAll() = 0;
+
+	//Reports the results of the tests
+	virtual void report()
+	{
+
+	}
+
+};
+
+
+
+
+
+#endif 
--- CORP/UNIT_TEST/Matrix/TEST_Matrix.hpp
+++ CORP/UNIT_TEST/Matrix/TEST_Matrix.hpp
@@ -1,17 +1,179 @@
 #ifndef _MATRIX_TESTER_HPP
 #define _MATRIX_TESTER_HPP
+#include <iostream>
+#include <string>
 #include "../../CORP/Matrix/Matrix.hpp"
 #include "CppUnitTest.h"
 //Unit tets for Matrix.hpp
 
+//using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+//{
 
-namespace MATRIXTEST
-{
-	template<typename T> class Matrix_Tester
+	template <typename T>
+	class MatrixTester
 	{
-		
-	};
-}
+	protected:
+		Matrix<T>* _m
 
+	public:
+
+		TEST_CLASS(IntMatrixTester)
+		{
+			_m = new Matrix<int>*;
+
+			runAll();
+
+			delete _m;
+		}
+
+		TEST_CLASS(DoubleMatrixTester)
+		{
+			_m = new Matrix<double>*;
+
+			runAll();
+
+			delete _m;
+		}
+
+		TEST_CLASS(BoolMatrixTester)
+		{
+			_m = new Matrix<bool>*;
+
+			runAll();
+
+			delete _m;
+		}
+
+		TEST_CLASS(UnsignedMatrixTester)
+		{
+			_m = new Matrix<unsigned>*;
+
+			runAll();
+
+			delete _m;
+		}
+
+		TEST_CLASS(FloatMatrixTester)
+		{
+			_m = new Matrix<float>*;
+
+			runAll();
+
+			delete _m;
+		}
+
+
+
+
+
+
+		TEST_METHOD(testConstructor)
+		{
+			_os << "\t" << "Test constructor..." << std::endl;
+
+			int errors = 0;
+
+			if (_m->size() != 0)
+			{
+				emitError("Constructor error; size is not zero");
+				errors++;
+			}
+			if (_m->empty())
+			{
+				emitError("Constructor error; array is empty");
+				errors++
+			}
+
+			else { return 0; }
+
+			check(_m, errors);
+
+		}
+
+		TEST_METHOD(testGet)
+		{
+			_m->get();
+		}
+
+		TEST_METHOD(testSet)
+		{
+			_m->set();
+		}
+
+		TEST_METHOD(testRemove)
+		{
+			_m->remove();
+		}
+
+		TEST_METHOD(testToString)
+		{
+			m->toString();
+		}
+
+		virtual ~MatrixTester()
+		{
+			_m->clear();
+		}
+
+		virtual runAll()
+		{
+			//Run Tests and reports
+			_m->clear();
+
+			testConstructor();
+			testGet();
+			testSet();
+			testRemove();
+			testToString();
+
+			//_errorCountMap["constructor"] = testConstructor();
+			//_m->clear();
+
+			//_errorCountMap["set"] = testSet();
+			//_m->clear();
+
+			//_errorCountMap["get"] = testGet();
+			//_m->clear();
+
+			//_errorCountMap["remove"] = testRemove();
+			//_m->clear();
+
+			//_errorCountMap["toString"] = testToString();
+			//_m->clear();
+		}
+
+		/*
+			MatrixTester(Matrix<T>* aMatrix, const std::string& name = "Adjacency Matrix", std::ostream& out = std::cout) : AbstractTester{ name, out }, _m{ aMatrix }
+			{
+
+			}
+
+
+
+			TEST_METHOD(testConstructor)
+			{
+				_os << "\t" << "Test constructor..." << std::endl;
+
+				int errors = 0;
+
+				if (_m->size() != 0)
+				{
+					emitError("Constructor error; size is not zero");
+					errors++;
+				}
+				if (_m->empty())
+				{
+					emitError("Constructor error; array is empty");
+					errors++
+				}
+				check(_m, errors);
+
+				_m->remove(Utilities::generate());
+
+				if (_m->contains(Utilities::generate()))
+			}
+		*/
+	};
+//}
 
 #endif // _MATRIX_TESTER_HPP
